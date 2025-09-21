198da7de16e366324942630b6e4c644de500cc3f
RichardoJermo <ricardo@terrawattenergy.in>
2025-09-21 10:41:32 +0200

    Update streamlit_finance.py

--- streamlit_finance.py
+++ streamlit_finance.py
@@ -63,7 +63,7 @@ fig = go.Figure(data=[go.Sankey(
     arrangement="snap",
     node=dict(
         pad=18,
-        thickness=10,
+        thickness=40,
         line=dict(color="black", width=0.4),
         label=labels,
         color=colors
@@ -133,5 +133,6 @@ try:
     st.image(logo, use_container_width=True)
 except:
     st.info("Please add your logo as 'logo.png' in the same directory to display it here.")
+
 
 
